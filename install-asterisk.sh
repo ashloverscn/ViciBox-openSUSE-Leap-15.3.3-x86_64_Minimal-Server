d7f4b80595a67b5f4b321ea7da540b096ba1a9cd
ashloverscn <ashloverscn@gmail.com>
2023-06-25 05:02:35 +0530

    Update install-asterisk.sh

--- install-asterisk.sh
+++ install-asterisk.sh
@@ -1,33 +1,29 @@
 #!/bin/sh
-ver=16.17.0
-oem=-vici
-subdr=beta-apps
-echo -e "\e[0;32m Install Asterisk v$ver$oem \e[0m"
+ver=3.2.0
+vici=0
+dahdi-linux-complete-$ver+$ver
+echo -e "\e[0;32m Install Dahdi Audio_CODEC Driver v$ver \e[0m"
 sleep 2
 cd /usr/src
-#rm -rf asterisk*
-#rpm -i --replacepkgs /var/cache/zypp/packages/openSUSE-Leap-15.3-ViciDial-Ast13/x86_64/asterisk-*
-#zypper remove -y asterisk
-#zypper remove -y asterisk-*
-zypper install -y asterisk
-zypper install -y asterisk-*
-wget -O asterisk-$ver$oem.tar.gz http://download.vicidial.com/$subdr/asterisk-$ver$oem.tar.gz
-#wget http://download.vicidial.com/beta-apps/asterisk-16.17.0-vici.tar.gz
-tar -xvzf asterisk-$ver$oem.tar.gz
-cd asterisk-$ver$oem
-
-: ${JOBS:=$(( $(nproc) + $(nproc) / 2 ))}
-./configure --libdir=/usr/lib64 --with-gsm=internal --enable-opus --enable-srtp --with-ssl --enable-asteriskssl --with-pjproject-bundled --with-jansson-bundled
-
-#### asterisk menuselect preconfig
-make menuselect/menuselect menuselect-tree menuselect.makeopts
-#enable app_meetme
-menuselect/menuselect --enable app_meetme menuselect.makeopts
-#enable res_http_websocket
-menuselect/menuselect --enable res_http_websocket menuselect.makeopts
-#enable res_srtp
-menuselect/menuselect --enable res_srtp menuselect.makeopts
-make -j ${JOBS} all
+#rm -rf dahdi-linux-complete*
+#yum remove dahdi* -y
+#yum remove dahdi-tools* -y
+yum install dahdi* -y
+yum install dahdi-tools* -y
+if [ $vici -eq 1 ]
+then
+	wget http://download.vicidial.com/required-apps/dahdi-linux-complete-2.3.0.1+2.3.0.tar.gz
+	tar -xvzf dahdi-linux-complete-2.3.0.1+2.3.0.tar.gz
+	cd dahdi-linux-complete-2.3.0.1+2.3.0
+else
+	wget -O dahdi-linux-complete-$ver+$ver.tar.gz https://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-$ver%2B$ver.tar.gz
+	tar -xvzf dahdi-linux-complete-$ver+$ver.tar.gz
+	cd dahdi-linux-complete-$ver+$ver
+fi
+make all
 make install
-make samples
-
+make config
+modprobe dahdi
+modprobe dahdi_dummy
+dahdi_genconf -v
+dahdi_cfg -v
