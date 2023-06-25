1636260b60c83b6f2bb8a2ac9a75d7fa1cbfaf8b
ashloverscn <ashloverscn@gmail.com>
2023-06-25 07:01:29 +0530

    Update install-asterisk.sh

--- install-asterisk.sh
+++ install-asterisk.sh
@@ -6,7 +6,7 @@ echo -e "\e[0;32m Install Dahdi Audio_CODEC Driver v$ver \e[0m"
 sleep 2
 cd /usr/src
 #rm -rf dahdi-linux-complete*
-#zypper remove -y asterisk asterisk-devel asterisk-dahdi asterisk-snmp asterisk-alsa asterisk-calendar asterisk-console asterisk-corosync asterisk-freetds asterisk-moh-base asterisk-odbc asterisk-perl asterisk-pgsql asterisk-radius asterisk-sounds-base asterisk-sounds-devel asterisk-spandsp libasteriskssl1
+#zypper remove -y asterisk asterisk-devel asterisk-dahdi asterisk-alsa asterisk-calendar asterisk-console asterisk-corosync asterisk-freetds asterisk-moh-base asterisk-odbc asterisk-perl asterisk-pgsql asterisk-radius asterisk-sounds-base asterisk-sounds-devel asterisk-spandsp libasteriskssl1
 zypper install -y asterisk asterisk-devel asterisk-dahdi asterisk-snmp asterisk-alsa asterisk-calendar asterisk-console asterisk-corosync asterisk-freetds asterisk-moh-base asterisk-odbc asterisk-perl asterisk-pgsql asterisk-radius asterisk-sounds-base asterisk-sounds-devel asterisk-spandsp libasteriskssl1
 #zypper remove -y dahdi-linux dahdi-linux-devel dahdi-linux-kmp-default dahdi-linux-kmp-preempt dahdi-tools
 zypper install -y dahdi-linux dahdi-linux-devel dahdi-linux-kmp-default dahdi-linux-kmp-preempt dahdi-tools
