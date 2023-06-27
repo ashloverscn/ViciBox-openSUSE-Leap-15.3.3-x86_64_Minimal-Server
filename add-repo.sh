d5ba1e576b4d1cfc6d6af3c0cb24d4e50adeba2a
ashloverscn <ashloverscn@gmail.com>
2023-06-27 08:43:40 +0530

    Update add-repo.sh

--- add-repo.sh
+++ add-repo.sh
@@ -3,7 +3,9 @@
 cd /usr/src
 ## remove all repos and add our requirment repo set for vicibox
 zypper rr --all
-zypper ar http://mirrorcache-us.opensuse.org/distribution/leap/15.3/repo/oss/ openSUSE-Leap-15.3-Oss
+
+zypper ar http://mirrorcache-us.opensuse.org/distribution/leap/$releasever/repo/oss/ openSUSE-Leap-15.3-Oss
+zypper ar http://mirrorcache-us.opensuse.org/update/leap/$releasever/oss/ openSUSE-Leap-15.3-Update
 #zypper ar http://mirrorcache-us.opensuse.org/repositories/devel:/languages:/perl/openSUSE_Leap_15.3/ openSUSE-Leap-15.3-PERL
 zypper ar http://mirrorcache-us.opensuse.org/repositories/devel:/languages:/perl/15.4/ openSUSE-Leap-15.4-PERL
 zypper ar http://mirrorcache-us.opensuse.org/repositories/home:/vicidial/openSUSE_Leap_15.3/ openSUSE-Leap-15.3-ViciDial
@@ -11,7 +13,7 @@ zypper ar http://mirrorcache-us.opensuse.org/repositories/home:/vicidial:/asteri
 zypper ar http://mirrorcache-us.opensuse.org/repositories/home:/vicidial:/asterisk-16/openSUSE_Leap_15.3/ openSUSE-Leap-15.3-ViciDial-Ast16
 zypper ar http://mirrorcache-us.opensuse.org/repositories/home:/vicidial:/vicibox/openSUSE_Leap_15.3/ openSUSE-Leap-15.3-ViciDial-ViciBox
 zypper ar http://mirrorcache-us.opensuse.org/repositories/devel:/languages:/php/openSUSE_Leap_15.4/ openSUSE-Leap-15.4-PHP-Applications
-zypper ar http://mirrorcache-us.opensuse.org/update/leap/15.3/oss/ openSUSE-Leap-15.3-Update
+
 zypper ar http://mirrorcache-us.opensuse.org/repositories/home:/zippy:/jx:/packages-ready/openSUSE_Leap_15.3/ openSUSE_Leap_15.3-zippy-jx
 ## refresh and trust repo keys and update
 zypper --gpg-auto-import-keys ref
